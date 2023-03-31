7287954b40313541c9721a24e8712511723c8dc6
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-03-31 22:35:11 +0530

    add assets.pokemon.com to next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.pokemon.com",
+      },
+    ],
+  },
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
