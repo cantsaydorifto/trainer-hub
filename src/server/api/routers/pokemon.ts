25b1538ad0f88a3bba28e8faa98821333f981457
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-04-01 21:00:27 +0530

    add team routers

--- src/server/api/routers/pokemon.ts
+++ src/server/api/routers/pokemon.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
@@ -51,4 +52,85 @@ export const pokemonRouter = createTRPCRouter({
     const caughtPokemon = res.map((el) => el.pokemonId);
     return caughtPokemon;
   }),
+
+  getTeam: protectedProcedure.query(async ({ ctx }) => {
+    const team = await ctx.prisma.team.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+      select: {
+        pokemonId: true,
+        name: true,
+        type: true,
+      },
+    });
+    let teamSpacesLeft = team.length;
+    while (teamSpacesLeft < 6) {
+      team.push({
+        name: "",
+        pokemonId: 0,
+        type: "",
+      });
+      teamSpacesLeft = teamSpacesLeft + 1;
+    }
+    return team;
+  }),
+
+  addToTeam: protectedProcedure
+    .input(
+      z.object({
+        pokemonId: z.number().lte(905).gte(1),
+        name: z.string(),
+        type: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const team = await ctx.prisma.team.findMany({
+        where: {
+          userId: ctx.session.user.id,
+        },
+      });
+      if (team.length > 6) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Team is full",
+        });
+      }
+      await ctx.prisma.team.create({
+        data: {
+          pokemonId: input.pokemonId,
+          name: input.name,
+          type: input.type,
+          user: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+    }),
+
+  removeFromTeam: protectedProcedure
+    .input(z.number().lte(905).gte(1))
+    .mutation(async ({ ctx, input }) => {
+      const team = await ctx.prisma.team.findMany({
+        where: {
+          userId: ctx.session.user.id,
+        },
+      });
+      if (team.length < 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Team is empty",
+        });
+      }
+      await ctx.prisma.team.delete({
+        where: {
+          userId_pokemonId: {
+            pokemonId: input,
+            userId: ctx.session.user.id,
+          },
+        },
+      });
+    }),
 });
