402d6b96e8f9a8432f6e2d666206221b930b06f1
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-04-01 23:52:35 +0530

    minor fix

--- src/components/team/updateTeam.tsx
+++ src/components/team/updateTeam.tsx
@@ -1,13 +1,13 @@
 import styles from "./updateTeam.module.css";
 
 type props = {
-  onClick: (pokemonId: number, idx: number) => void;
   addPokemon: boolean;
+  onClick: () => void;
 };
 
 export default function UpdateTeam({ onClick, addPokemon }: props) {
   return addPokemon ? (
-    <button className={styles.remove} onClick={() => onClick}>
+    <button className={styles.remove} onClick={onClick}>
       <img
         className={styles.removeIcon}
         src="https://cdn-icons-png.flaticon.com/512/484/484662.png"
@@ -15,7 +15,7 @@ export default function UpdateTeam({ onClick, addPokemon }: props) {
       />
     </button>
   ) : (
-    <button className={styles.add} onClick={() => onClick}>
+    <button className={styles.add} onClick={onClick}>
       +
     </button>
   );
