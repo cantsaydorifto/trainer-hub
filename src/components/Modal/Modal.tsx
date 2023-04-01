11b8d9e6a85b518e7c867be1bcf6ebd8740a4ad9
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-04-01 21:00:46 +0530

    add modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,41 @@
+import { motion } from "framer-motion";
+import { Roboto } from "next/font/google";
+import { checkStyleClass } from "~/helpers/checkStyleClass";
+import styles from "./modal.module.css";
+
+const modalHandler = (
+  e: React.MouseEvent<HTMLDivElement>,
+  toggleModal: () => void
+) => {
+  if (e.target === e.currentTarget) toggleModal();
+};
+
+const roboto = Roboto({
+  weight: ["300"],
+  subsets: ["latin"],
+});
+
+type modalProps = {
+  modalContent: string | undefined;
+  toggleModal: () => void;
+  children: React.ReactNode;
+};
+
+export default function Modal(props: modalProps) {
+  return (
+    <div
+      onClick={(event) => modalHandler(event, props.toggleModal)}
+      className={`${checkStyleClass(styles.overlay)} ${roboto.className}`}
+    >
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        className={`${checkStyleClass(styles.content)} ${checkStyleClass(
+          props.modalContent
+        )}`}
+      >
+        {props.children}
+      </motion.div>
+    </div>
+  );
+}
